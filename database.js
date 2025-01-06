1e7d320afa3c51d38e99a28b7483218dc57a97de
haidar <haidarah134@gmail.com>
2025-01-06 20:55:25 +0700

    update path database sebagai submodul

--- database.js
+++ database.js
@@ -1,14 +1,15 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database.sqlite');
+// berjalan sebagai submodule projek laravel
+const db = new sqlite3.Database('../database/database.sqlite');
 
-// Create a table if it doesn't exist
-db.run(`
-  CREATE TABLE IF NOT EXISTS motors (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    model TEXT NOT NULL,
-    tahun INTEGER NOT NULL,
-    owner TEXT
-  )
-`);
+// // Create a table if it doesn't exist
+// db.run(`
+//   CREATE TABLE IF NOT EXISTS motors (
+//     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     model TEXT NOT NULL,
+//     tahun INTEGER NOT NULL,
+//     owner TEXT
+//   )
+// `);
 
 module.exports = db;
